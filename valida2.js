6e24409cd873a071acbeddf52ffb813baaee9cb5
SofiQuiroz <88165227+SofiQuiroz@users.noreply.github.com>
2021-10-31 17:23:13 -0300

    Add files via upload

--- valida2.js
+++ valida2.js
@@ -7,7 +7,7 @@ function iniciar() {
 function valida_nombre() {
   var elemento = document.getElementById('nombre');
   if (elemento.value ==''){
-    alert('El campo nombre no puede estar vacío');
+    alert('El campo nombre no puede estar vacío.');
     return false
   }
   return true;
@@ -16,16 +16,19 @@ function valida_nombre() {
 function valida_telefono() {
 var elemento = document.getElementById('tel');
   if (isNaN(elemento.value)){
-    alert('El campo teléfono debe ser un número');
+    alert('El campo teléfono debe ser un número.');
     return false
   }
   return true;
 }
 
 function valida_check() {
-var elemento = document.getElementById('mayor');
-if (!elemento.checked){
-  alert('Debe ser mayor de edad');
+var elem1 = document.getElementById('check1');
+var elem2 = document.getElementById('check2');
+var elem3 = document.getElementById('check3');
+var elem4 = document.getElementById('check4');
+if (!elem1.checked && !elem2.checked && !elem3.checked && !elem4.checked){
+  alert('Debe seleccionar al menos uno.');
   return false;
 }
 return true;
@@ -35,8 +38,6 @@ function validate(e){
   if(valida_nombre() && valida_telefono() && valida_check() && confirm('Desea enviar?')){
     return true;
   } else {
-    // evito que se envíe el formulario
-    // traba el evento que por defecto se disparó con el botón enviar.
     e.preventDefault();
     return false;
   }
